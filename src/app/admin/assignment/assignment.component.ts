6594fbf4944485509380c42bf9c5e92c22652cf2
tungtran <tungtt309@gmail.com>
2020-06-09 00:12:37 +0700

    add date filter

--- src/app/admin/assignment/assignment.component.ts
+++ src/app/admin/assignment/assignment.component.ts
@@ -4,6 +4,8 @@ import { MatSort } from '@angular/material/sort';
 import { SelectionModel } from '@angular/cdk/collections';
 import { MatDialog } from '@angular/material/dialog';
 import { DatePipe } from '@angular/common';
+import { NativeDateAdapter, MatDateFormats, DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 
 export interface AssignmentData {
@@ -16,17 +18,48 @@ export interface AssignmentData {
     status : string;
 }
 
+export class AppDateAdapter extends NativeDateAdapter {
+  format(date: Date, displayFormat: Object): string {
+    if (displayFormat === 'input') {
+      let day: string = date.getDate().toString();
+      day = +day < 10 ? '0' + day : day;
+      let month: string = (date.getMonth() + 1).toString();
+      month = +month < 10 ? '0' + month : month;
+      let year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+    }
+    return date.toDateString();
+  }
+}
+
+export const APP_DATE_FORMATS: MatDateFormats = {
+  parse: {
+    dateInput: { month: 'short', year: 'numeric', day: 'numeric' },
+  },
+  display: {
+    dateInput: 'input',
+    monthYearLabel: { year: 'numeric', month: 'numeric' },
+    dateA11yLabel: { year: 'numeric', month: 'long', day: 'numeric'
+    },
+    monthYearA11yLabel: { year: 'numeric', month: 'long' },
+  }
+};
+
 @Component({
   selector: 'assignment-tables',
   templateUrl: './assignment.component.html',
-  styleUrls: ['./assignment.component.scss']
+  styleUrls: ['./assignment.component.scss'],
+  providers: [
+    {provide: DateAdapter, useClass: AppDateAdapter},
+    {provide: MAT_DATE_FORMATS, useValue: APP_DATE_FORMATS}
+  ]
 })
 
 
 export class AssignmentComponent implements OnInit, AfterViewInit {
   displayedColumns = ['userId', 'startDate', 'completeDate', 'location', 'description', 'column', 'status'];
   dataSource: Array<AssignmentData>;
-  dataTable : Array<AssignmentData>
+  dataTable : MatTableDataSource<AssignmentData>
   selection: SelectionModel<AssignmentData>;
   startDate : Date;
   endDate : Date;
@@ -55,21 +88,44 @@ export class AssignmentComponent implements OnInit, AfterViewInit {
                       ,{userId : 10, startDate : '25/05/2020', completeDate : '07/06/2020', location : "Đại học Bách Khoa", description : "Thay biến áp", column : "Q3T2", status : "Đã hoàn thành"}
                       ,{userId : 2, startDate : '22/05/2020', completeDate : '04/06/2020', location : "Đại học Xây Dựng", description : "Hàn mối nối", column : "Q3T2", status : "Đã hoàn thành"}
                       ,{userId : 5, startDate : '22/05/2020', completeDate : '', location : "Đại học Xây Dựng", description : "Hàn mối nối", column : "Q3T2", status : "Đang kiểm tra"}];
-    this.dataTable = this.dataSource;
+    this.dataTable = new MatTableDataSource(this.dataSource);
   }
 
   ngAfterViewInit() {
+    this.dataTable.paginator = this.paginator;
+    this.dataTable.sort = this.sort;
+  }
 
+  isSmaller(date : Date, dateString : string) : boolean {
+    let fulldate = dateString.split('/');
+    let dateTime = new Date(Number(fulldate[2]), Number(fulldate[1]) - 1, Number(fulldate[0]));
+    return date == null ? true : date <= dateTime;
+  }
+
+  isLarger(date : Date, dateString : string) : boolean {
+    let fulldate = dateString.split('/');
+    let dateTime = new Date(Number(fulldate[2]), Number(fulldate[1]) - 1, Number(fulldate[0]));
+    return date == null ? true : date >= dateTime;
   }
 
   applyFilter() {
-      
+    console.log(this.userId);
+    let newData = [];
+    for (var row of this.dataSource) {
+        if (this.userId == row.userId || this.userId == null) {
+          if (this.isSmaller(this.startDate, row.startDate) && this.isLarger(this.endDate, row.startDate)) {
+              newData.push(row);
+          }
+        }
+    }
+    this.dataTable.data = newData;
+    this.dataTable._updateChangeSubscription();
   }
 
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
     const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.length;
+    const numRows = this.dataTable.data.length;
     return numSelected === numRows;
   }
 
